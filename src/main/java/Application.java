e9eb44e247901bd81405f1a27a8eb871aeb55d45
jerry <zeroumbin@gmail.com>
2021-10-12 00:04:30 -0300

    add kafka consumer

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,29 +1,45 @@
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.util.List;
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
+
+
 
 public class Application {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         System.out.println("enviando");
         var producer = new KafkaProducer<String, String>(properties());
-        String value = "id200";
-        var record = new ProducerRecord("ECOMMERCE_NEW_ORDER", value, value);
-
         Callback callback = (data, ex) -> {
             if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
 
-            System.out.println("enviado para: " + data.topic());
+            System.out.println("enviado para: " + data.topic() + " OFFSET::: " + data.offset() + " PARTITION::: " + data.partition() );
         };
 
-        producer.send(record, callback).get();
+        for (int i = 0; i < 2; i++) {
+            String value = UUID.randomUUID().toString();
+            var record = new ProducerRecord("ECOMMERCE_NEW_ORDER", value, value);
+            producer.send(record, callback).get();
+        }
+
+        try (AdminClient adminClient = AdminClient.create(properties())) {
+            try {
+                var result = adminClient.deleteTopics(List.of("ECOMMERCE_NEW_ORDER"));
+                result.wait();
+            } catch (final Exception e) {
+                throw new RuntimeException("Failed :", e);
+            }
+        }
 
     }
 
